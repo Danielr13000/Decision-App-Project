a8a088f71cad3367526949aa6c7951dc54e4bb3d
Danielr13000 <danielr726@nycstudents.net>
2026-04-17 08:40:18 -0400

    Add files via upload

---
+++ Decision App Project/script.js
@@ -0,0 +1,26 @@
+let button = document.querySelector(".button1");
+
+let output = document.querySelector(".output");
+
+let x = Math.floor((Math.random() * 12) + 1);
+
+let genres;
+
+
+
+
+
+fetch("genre.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        // handle ten random cards
+        console.log(response);
+        genres=response;
+        button.addEventListener("click", function () {
+            x = Math.floor((Math.random() * 12));
+            console.log(x);
+            output.innerHTML = "Genre is "+ genres[x].genre;
+        })
+    })
